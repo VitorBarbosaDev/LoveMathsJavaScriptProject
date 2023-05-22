d194019d6d5b6c642f4f79767445c06231e4dde1
VitorBarbosa1 <vitor.barbosasilva@mail.itsligo.ie>
2023-05-22 01:16:20 +0100

    Setup Game Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-﻿
+﻿function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+}
+
+function displaySubtractQuestion(operand1, operand2) {
+
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+
+}
